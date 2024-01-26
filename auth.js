c4b505b0a454e431bae289d4c68e2a2880d9beea
Mauro <mauro.avon@insiel.it>
2024-01-26 16:36:51 +0100

    Added authentication

---
+++ auth.js
@@ -0,0 +1,13 @@
+function authenticate(login, password) {
+
+	if (login === 'login' && password === 'password') {
+	
+		return 'You are logged in';
+	
+	} else {
+	
+		return 'Login is not correct';
+	
+	}
+	
+}
